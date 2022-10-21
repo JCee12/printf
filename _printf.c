35f76fa697eb39b8c2dabdf94062ed92a035dc79
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-10-21 13:48:10 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,82 +1,76 @@
 #include "main.h"
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
+ * print_op - function to check which specifier to print
+ * @format: string being passed
+ * @print_arr: array of struct ops
+ * @list: list of arguments to print
+ * Return: numb of char to be printed
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+int print_op(const char *format, fmt_t *print_arr, va_list list)
 {
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
+	char a;
+	int count = 0, b = 0, c = 0;
 
-	for (i = 0; p[i].t != NULL; i++)
+	a = format[b];
+	while (a != '\0')
 	{
-		if (*(p[i].t) == *format)
+		if (a == '%')
 		{
-			break;
+			c = 0;
+			b++;
+			a = format[b];
+			while (print_arr[c].type != NULL &&
+			       a != *(print_arr[c].type))
+				c++;
+			if (print_arr[c].type != NULL)
+				count = count + print_arr[c].f(list);
+			else
+			{
+				if (a == '\0')
+					return (-1);
+				if (a != '%')
+					count += _putchar('%');
+				count += _putchar(a);
+			}
 		}
+		else
+			count += _putchar(a);
+		b++;
+		a = format[b];
 	}
-	return (p[i].f);
+	return (count);
 }
 
 /**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
+ * _printf - prints output according to format
+ * @format: string being passed
+ * Return: char to be printed
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	va_list list;
+	int a = 0;
+
+	fmt_t ops[] = {
+		{"c", ch},
+		{"s", str},
+		{"d", _int},
+		{"b", _bin},
+		{"i", _int},
+		{"u", _ui},
+		{"o", _oct},
+		{"x", _hex_l},
+		{"X", _hex_u},
+		{"R", _rot13},
+		{NULL, NULL}
+	};
 
 	if (format == NULL)
 		return (-1);
-	va_start(valist, format);
-	while (format[i])
-	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
-	}
-	va_end(valist);
-	return (count);
+	va_start(list, format);
+	a = print_op(format, ops, list);
+	va_end(list);
+	return (a);
 }
