f337a3ad6252c93a72a3b4512e4dced66624bccf
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-10-21 13:43:24 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,33 +1,33 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ * struct fmt - function to check for formats
+ * @type: The format to print
+ * @f: The print function to use
  */
-typedef struct print
+typedef struct fmt
 {
-	char *t;
-	int (*f)(va_list);
-} print_t;
+	char *type;
+	int (*f)();
+} fmt_t;
 
-int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
-
-#endif  /* _MAIN_H */
-
+int print_op(const char *format, fmt_t *print_arr, va_list list);
+int ch(va_list character);
+int str(va_list string);
+int _int(va_list integ);
+int _ui(va_list unsign);
+int _oct(va_list octo);
+int _rot13(va_list rot);
+int _hex_str(unsigned int n, unsigned int hex, char alpha);
+int _hex_l(va_list hexa);
+int _hex_u(va_list hexa);
+int _strlen(char *s);
+int _bin(va_list bin);
+int _putchar(char c);
+#endif
